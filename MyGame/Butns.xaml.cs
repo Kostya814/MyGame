bbe0ea25581f38508821d9226f29b35afca045cb
Kostya814 <112933462+Kostya814@users.noreply.github.com>
2023-04-03 09:48:13 +0700

    Добавьте файлы проекта.

---
+++ MyGame/Butns.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyGame
+{
+    /// <summary>
+    /// Логика взаимодействия для Butns.xaml
+    /// </summary>
+    public partial class Butns : StackPanel
+    {
+        private MainWindow parent;
+        public Butns()
+        {
+            InitializeComponent();
+        }
+
+        public Butns(int points) : this()
+        {
+
+        }
+
+        private void Click100(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MyGame/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class Question
+    {
+        private string _question;
+        private string _answer;
+        private Category _category;
+        public Question(string question, string answer,Category category)
+        { 
+            _question = question;
+            _answer = answer;
+            _category = category;
+        }
+    }
+}
